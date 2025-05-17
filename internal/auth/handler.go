25d5b742901420783cb007ee9b1af5009539a1c0
Eugene Dilbarov <eugene.dilbarov@gmail.com>
2025-05-17 01:04:28 +0000

    feat: add simple response

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"encoding/json"
 	"link-shorter/configs"
 	"log"
 	"net/http"
@@ -25,6 +26,16 @@ func NewHandler(router *http.ServeMux, deps HandlerDeps) {
 func (h *Handler) Login() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		log.Printf("auth")
+		res := LoginResponse{
+			Token: "123",
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		err := json.NewEncoder(w).Encode(res)
+		if err != nil {
+			return
+		}
 	}
 }
 
