60908859058d7f3b9d76d468a5471b4b7b457d16
Eugene Dilbarov <eugene.dilbarov@gmail.com>
2025-06-08 19:22:11 -0400

    refactor: change types in factory

--- pkg/cqrs/providers/in-memory/factory.go
+++ pkg/cqrs/providers/in-memory/factory.go
@@ -1,5 +1,7 @@
 package cqrs
 
-func InitInMemory() (*InMemoryCommandBus, *InMemoryQueryBus, *InMemoryEventBus) {
+import cqrsInterfaces "link-shorter/pkg/cqrs/interfaces"
+
+func InitInMemory() (cqrsInterfaces.CommandBus, cqrsInterfaces.QueryBus, cqrsInterfaces.EventBus) {
 	return NewInMemoryCommandBus(), NewInMemoryQueryBus(), NewInMemoryEventBus()
 }
