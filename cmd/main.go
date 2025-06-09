48989300713f319e3ef5710374991433a0e67211
Eugene Dilbarov <eugene.dilbarov@gmail.com>
2025-06-08 20:00:53 -0400

    feat: add click and lick domains to main

--- cmd/main.go
+++ cmd/main.go
@@ -7,12 +7,15 @@ import (
 	"link-shorter/configs"
 	authHandlers "link-shorter/internal/auth/handlers"
 	authServices "link-shorter/internal/auth/services"
+	clickRepository "link-shorter/internal/click/repository"
+	clickServices "link-shorter/internal/click/services"
 	linkHandlers "link-shorter/internal/link/handlers"
 	linkRepository "link-shorter/internal/link/repository"
 	linkServices "link-shorter/internal/link/services"
 	userHandlers "link-shorter/internal/user/handlers"
 	userRepository "link-shorter/internal/user/repository"
 	userServices "link-shorter/internal/user/services"
+	inMemoryProvider "link-shorter/pkg/cqrs/providers/in-memory"
 	"link-shorter/pkg/db"
 	"link-shorter/pkg/jwt"
 	"link-shorter/pkg/logger"
@@ -22,24 +25,34 @@ import (
 
 func main() {
 	conf := configs.LoadConfig()
-
 	log.Logger = logger.SetupLogger(conf.App.Env)
-
 	database := db.NewDb(&conf.Db)
-
 	router := http.NewServeMux()
+	commandBus, queryBus, eventBus := inMemoryProvider.InitInMemory()
 
 	// Repositories
 	linkRepo := linkRepository.NewPostgresLinkRepository(database)
 	userRepo := userRepository.NewPostgresUserRepository(database)
+	clickRepo := clickRepository.NewPostgresClickRepository(database)
 
 	// Services
-	linkService := linkServices.NewServiceFacade(linkRepo)
 	userService := userServices.NewServiceFacade(userRepo)
 	jwtService := jwt.NewJWTService(conf.Auth.Secret)
 	authService := authServices.NewAuthService(userRepo, jwtService)
+	linkService := linkServices.NewServiceFacade(linkServices.ServiceFacadeDeps{
+		CommandBus:     commandBus,
+		QueryBus:       queryBus,
+		EventBus:       eventBus,
+		LinkRepository: linkRepo,
+	})
+	_ = clickServices.NewServiceFacade(clickServices.ServiceFacadeDeps{
+		CommandBus:      commandBus,
+		QueryBus:        queryBus,
+		EventBus:        eventBus,
+		ClickRepository: clickRepo,
+	})
 
-	// Handler
+	// Handlers
 	authHandlers.NewHandler(router, authHandlers.HandlerDeps{
 		Config:      conf,
 		AuthService: authService,
